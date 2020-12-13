e65a23d35f153b9b22245643ca04b944cca75741
nguyenle <nguyenle@nguyens-macbook-pro.home>
2020-12-13 10:51:55 +0100

    Spaceship Sprite Object

---
+++ classes/src/classes/Spaceship.java
@@ -0,0 +1,30 @@
+package classes;
+
+import java.awt.Image;
+import java.util.Random;
+
+import util.FileUtil;
+
+public class Spaceship {
+	static Random randomGenerator = new Random();
+	
+	Image spaceship;
+	Image fromLeft;
+	Image fromRight;
+	Image topLeft;
+	Image topRight;
+	
+	double gradient = 0;
+	
+	Spaceship()
+	{
+		spaceship = FileUtil.loadImage("/Users/nguyenle/Starsfield-spaceship-game-graphics/rocket.png");
+		fromLeft = FileUtil.loadImage("/Users/nguyenle/Starsfield-spaceship-game-graphics/bottomleft.png"); 
+		fromRight = FileUtil.loadImage("/Users/nguyenle/Starsfield-spaceship-game-graphics/bottomright.png"); 
+		topLeft = FileUtil.loadImage("/Users/nguyenle/Starsfield-spaceship-game-graphics/topleft.png"); 
+		topRight = FileUtil.loadImage("/Users/nguyenle/Starsfield-spaceship-game-graphics/topright.png"); 
+	}
+	
+	
+}
+
